0012c3e987fff460afc7e5f7bd2df2021c6a887d
fuduhui <382735550@qq.com>
2019-03-06 15:56:13 +0800

    poi回调

--- src/test/poi/CellValCallBack.java
+++ src/test/poi/CellValCallBack.java
@@ -1,5 +1,11 @@
 package poi;
 
 public abstract class CellValCallBack {
+    /**
+     * 设置cell值的时候回调
+     * @param dadaRowIndex 第几行数据,从0开始
+     * @param cellIndex 一行里面的第几个cell,从0开始
+     * @return
+     */
     abstract String getCellVal(int dadaRowIndex,int cellIndex);
 }
